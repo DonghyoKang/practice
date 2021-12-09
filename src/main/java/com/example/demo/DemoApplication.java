0173c682e1a35b44e913ca050951f47734902ba1
donghyo <newtype1595@gmail.com>
2021-12-09 22:08:18 +0900

    Initial Commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+@EnableJpaAuditing
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Bean
+	public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
+		return new HiddenHttpMethodFilter();
+	}
+}
---
+++ src/main/java/com/example/demo/controller/BoardController.java
@@ -0,0 +1,124 @@
+package com.example.demo.controller;
+
+import com.example.demo.dto.BoardDto;
+import com.example.demo.dto.FileDto;
+import com.example.demo.service.BoardService;
+import com.example.demo.service.FileService;
+import com.example.demo.util.MD5Generator;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+@Controller
+public class BoardController {
+    private BoardService boardService;
+    private FileService fileService;
+
+    public BoardController(BoardService boardService, FileService fileService) {
+        this.boardService = boardService;
+        this.fileService = fileService;
+    }
+
+    @GetMapping("/")
+    public String list(Model model) {
+        List<BoardDto> boardDtoList = boardService.getBoardList();
+        model.addAttribute("postList", boardDtoList);
+        return "board/list";
+    }
+
+    @GetMapping("/post")
+    public String post(){
+        return "board/post";
+    }
+
+    @PostMapping("/post")
+    public String write(@RequestParam("file") MultipartFile files, BoardDto boardDto) {
+        try {
+            if(!files.isEmpty()) {
+                String origFilename = files.getOriginalFilename();
+                String filename = new MD5Generator(origFilename).toString();
+                String savePath = System.getProperty("user.dir") + File.separator + "files";
+                if (!new File(savePath).exists()) {
+                    try {
+                        new File(savePath).mkdir();
+                    } catch (Exception e) {
+                        e.getStackTrace();
+                    }
+                }
+                String filePath = savePath + File.separator + filename;
+                files.transferTo(new File(filePath));
+
+                FileDto fileDto = new FileDto();
+                fileDto.setOrigFilename(origFilename);
+                fileDto.setFilename(filename);
+                fileDto.setFilePath(filePath);
+
+                Long fileId = fileService.saveFile(fileDto);
+                boardDto.setFileId(fileId);
+            }
+            boardService.savePost(boardDto);
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+        return "redirect:/";
+    }
+
+    @GetMapping("/post/{id}")
+    public String detail(@PathVariable("id") Long id, Model model) {
+        BoardDto boardDto = boardService.getPost(id);
+        model.addAttribute("post", boardDto);
+        String filename = fileService.getFile(boardDto.getFileId()).getOrigFilename();
+        model.addAttribute("filename", filename);
+        return "board/detail";
+    }
+
+    @GetMapping("/post/edit/{id}")
+    public String edit(@PathVariable("id") Long id, Model model) {
+        BoardDto boardDto = boardService.getPost(id);
+        model.addAttribute("post", boardDto);
+        return "board/edit";
+    }
+
+    @PutMapping("/post/edit/{id}")
+    public String update(BoardDto boardDto) {
+        boardService.savePost(boardDto);
+        return "redirect:/";
+    }
+
+    @DeleteMapping("/post/{id}")
+    public String delete(@PathVariable("id") Long id) {
+        BoardDto boardDto = boardService.getPost(id);
+        Long fileId = boardDto.getFileId();
+        boardService.deletePost(id);
+        fileService.deleteFile(fileId);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/download/{fileId}")
+    public ResponseEntity<Resource> fileDownload(@PathVariable("fileId") Long fileId) throws IOException {
+        FileDto fileDto = fileService.getFile(fileId);
+        Path path = Paths.get(fileDto.getFilePath());
+        Resource resource = new InputStreamResource(Files.newInputStream(path));
+        String filename = new String(fileDto.getOrigFilename().getBytes(StandardCharsets.UTF_8), StandardCharsets.ISO_8859_1);
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType("application/octet-stream"))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + filename + "\"")
+                .body(resource);
+    }
+}
